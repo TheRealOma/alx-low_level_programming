9f93f901f4886d2ad5ef0c96a019e432f9cef207
TheRealOma <obiamakaagbazue@gmail.com>
2022-06-04 19:29:10 -0700

    Where did my four go

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char putt[] = "Programming is like building a multilingual puzzle";
+	
+	puts (putt);
+
+	return 0;
+}
