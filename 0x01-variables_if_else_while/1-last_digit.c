34f55b96a6752d20e140cfc4b5ad38a1830f2677
TheRealOma <obiamakaagbazue@gmail.com>
2022-06-06 18:19:00 -0700

    Pseudocoding

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,14 +11,14 @@
 int main(void)
 {
 	int n;
-
+	int last_digit;
+	
 	srand(time(0));
 
 	n = rand() - RAND_MAX / 2;
 
-
-	int last_digit;
-        last_digit = n % 10;
+       
+       	last_digit = n % 10;
 
 
 	if (last_digit > 5)
