85ace425d7734675f09bb0397b8c7fdd6830eb80
TheRealOma <obiamakaagbazue@gmail.com>
2022-06-07 10:10:13 -0700

    Ratings

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x = 97;
+
+	while (x <= 122)
+	{
+		if (x!=113 || x!=101);
+		{
+			putchar(x);
+		}
+		x++;
+	}
+	putchar(10);
+	return (0);
+}
