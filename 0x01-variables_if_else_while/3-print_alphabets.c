bb5ad80e73055ada8ba172155ff8865b5c674ed5
TheRealOma <obiamakaagbazue@gmail.com>
2022-06-07 09:37:45 -0700

    Chess

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x;
+	x = 65;
+	while (x <= 90)
+	{
+		putchar(x);
+		x++;
+	}
+	putchar(10);
+	return 0;
+}
