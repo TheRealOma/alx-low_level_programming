9da556f03722fb22f6342c661a0d0bf2298102c3
TheRealOma <obiamakaagbazue@gmail.com>
2022-06-07 09:32:44 -0700

    Commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -10,11 +10,12 @@
 
 int main(void)
 {
-	for(int x = 'A'; x <= 'Z'; x++)
+	int x;
+	x = 65;
+	while (x <= 90)
 	{
-	        int lower_x = tolower(x);
-		putchar(lower_x);
+		putchar(x);
+		x++;
 	}
-	putchar('\n');
 	return 0;
 }
