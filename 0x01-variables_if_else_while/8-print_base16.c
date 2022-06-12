a2ddae6679a56c2c8cfa89e53108d965084591a4
TheRealOma <obiamakaagbazue@gmail.com>
2022-06-12 08:39:13 -0700

    Thread

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int y = 48;
+	int x = 97;
+
+	while (y <=57)
+	{
+		putchar(y);
+		y++;
+	}
+
+	while (x <= 102)
+	{
+		putchar(x);
+		x++;
+	}
+	putchar(10);
+	return (0);
+}
