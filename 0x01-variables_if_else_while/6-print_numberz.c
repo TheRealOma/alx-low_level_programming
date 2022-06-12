225cb9c2834f824d981a92cc042b4340120888d0
TheRealOma <obiamakaagbazue@gmail.com>
2022-06-12 07:57:31 -0700

    Not a mistake

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x = 48;
+
+	while (x <= 57)
+	{
+		putchar(x);
+		x++;
+	}
+	putchar(10);
+	return (0);
+}
