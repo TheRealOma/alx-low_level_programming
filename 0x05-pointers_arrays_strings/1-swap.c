8c806b0507a4c38c5f5facd2c73dea85e5fee42c
TheRealOma <obiamakaagbazue@gmail.com>
2022-06-14 01:24:24 -0700

    Documentation changes

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -6,10 +6,10 @@
  * Results: same as always
  */
 
-void swap_int(int *a, int *b)
+void swap_int(int *a, int *b) /**This is the function*/
 {
-	int random_variable = *a;
+	int random_variable = *a; /** Assigning a random variable to a*/
 	*a = *b;
-	*b = random_variable;
+	*b = random_variable; /** Now b becomes the random variable*/
 
 }
