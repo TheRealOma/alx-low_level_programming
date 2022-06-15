a14974abf328b598ce20d733e83f8436aba0dba5
TheRealOma <obiamakaagbazue@gmail.com>
2022-06-15 09:04:24 -0700

    Another directory

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *  main - check the code
+ *
+ *  Return: Always 0.
+ */
+
+int main(void)
+{
+	int x = 97;
+
+	while (x <= 122)
+	{
+		putchar(x);
+		x++;
+	}
+	putchar(10);
+	return (0);
+}
