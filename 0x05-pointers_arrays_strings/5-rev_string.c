19a5e0d05a18df1542d95ed178e9e063e34bc316
TheRealOma <obiamakaagbazue@gmail.com>
2022-06-16 13:41:27 -0700

    Lets see

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_rev - prints strings in reverse
+ * @s: string used
+ *
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int i = 0;
+
+	while (*(s + i))
+		i++;
+	i = i - 1;
+	while (i >= 0)
+	{
+		_putchar(*(s + i));
+		i--;
+	}
+}
