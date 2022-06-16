89d9afc1cbd2b96736982e9f27c8ddeeb1a0a89d
TheRealOma <obiamakaagbazue@gmail.com>
2022-06-16 16:01:54 -0700

    Omooooo

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,13 +12,13 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int i;
+	int i, n;
 
 	for (i = 0; src[i] != '\0'; i++)
 	{
-		dest[i] = src[i];
+		dest[n + i] = src[i];
 	}
-	dest[i] = '\0';
+	dest[i] != '\0';
 
 	return (dest);
 }
