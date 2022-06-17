f7270b35b47bec0cd48b49d70177eb1a6e70c72a
TheRealOma <obiamakaagbazue@gmail.com>
2022-06-16 17:31:27 -0700

    Okay

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -7,13 +7,18 @@
  * @dest: destination file
  * @src: source to be appended
  *
+ * Return: return destination
  */
 
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
+	int dest_length = 0;
 
-	for (i=0; i < n && src[i] != '\0'; i ++)
+	while (dest[dest_length] != '\0')
+		dest_length++;
+
+	for (i=0; i < n; i ++)
 		dest[n + i] = src[i];
 	dest[n+i] = '\0';
 
