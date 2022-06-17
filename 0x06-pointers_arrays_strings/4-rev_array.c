991561c771b2c482559743132161a51165a69a92
TheRealOma <obiamakaagbazue@gmail.com>
2022-06-16 19:48:22 -0700

    Commit message

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * reverse_array - reverse the array
+ * @a: an integer
+ * @n: another integer
+ *
+ * Return: return statement
+ */
+
+void reverse_array(int *a, int n)
+{
+	int h = n/2;
+	int f, r, i;
+
+	n -= 1;
+
+	for (i = 0; i < h; i ++)
+	{
+		f = a[n - i];
+		r = a[i];
+		a[i] = f;
+		a[n - i] = r;
+	}
+}
