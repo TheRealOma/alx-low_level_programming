e54cd45800ecd5dbebebc18b63b07af5c06b53a6
TheRealOma <obiamakaagbazue@gmail.com>
2022-06-17 16:46:33 -0700

    pointer

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,16 +3,10 @@
 /**
  * reset_to_98 - my function name which should reset the pointer to 98
  *@n: arguement for pointer
- *@ptr: pointer to be reset
-
- * Results: New pointer equal to 98 
+ * Return: New pointer equal to 98 
  */
 
 void reset_to_98(int *n)
 {
-	int *n;
-	int ptr;
-
-	*n = &ptr;
 	*n = 98;
 }
