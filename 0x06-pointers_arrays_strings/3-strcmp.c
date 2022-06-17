8e038abb7389e6c179c4b9f093683d5cefdd288e
TheRealOma <obiamakaagbazue@gmail.com>
2022-06-17 08:48:59 -0700

    It worked

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,21 +6,17 @@
  * @s1: string one
  * @s2: second string
  *
+ * Return: difference in length
  */
 
 int _strcmp(char *s1, char *s2)
 {
-	int i;
-	int n;
-
-	i = 0;
-	n = 0;
-
 	while (*s1 && (*s1 == *s2))
 	{
 		s1++;
 		s2++;
 	}
+
 	return (*s1 - *s2);
 }
 
